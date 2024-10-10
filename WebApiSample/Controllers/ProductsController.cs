2158921cebdc8c715c796c766abfe225cbcef84e
ardasenbaklavaci <107650690+ardasenbaklavaci@users.noreply.github.com>
2024-10-10 15:50:59 +0300

    Init
    Init

---
+++ WebApiSample/Controllers/ProductsController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApiSample.Models;
+
+namespace WebApiSample.Controllers
+{
+    public class ProductsController : Controller
+    {
+        WebApiSampleContext context;
+        public ProductsController(WebApiSampleContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: api/products
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> GetProducts()
+        {
+            var products = await context.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        // GET: api/products/5
+        [HttpGet("{id}")]
+        [Authorize]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var product = await context.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        /*
+        public IActionResult Index()
+        {
+            return View();
+        } */
+    }
+}
---
+++ WebApiSample/Controllers/TokenController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace WebApiSample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public TokenController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost("generate")]
+        public IActionResult GenerateToken([FromBody] UserCredentials credentials)
+        {
+            if (credentials.Username == "test" && credentials.Password == "password")
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.ASCII.GetBytes(_configuration["JwtSettings:Secret"]);
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new[] {
+                    new Claim(ClaimTypes.Name, credentials.Username)
+                }),
+                    Expires = DateTime.UtcNow.AddMinutes(int.Parse(_configuration["JwtSettings:ExpirationInMinutes"])),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+                    Issuer = _configuration["JwtSettings:Issuer"],
+                    Audience = _configuration["JwtSettings:Audience"]
+                };
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                return Ok(new { token = tokenHandler.WriteToken(token) });
+            }
+
+            return Unauthorized();
+        }
+    }
+    public class UserCredentials
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
